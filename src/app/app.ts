1cba4e177378b5fca191ba8b644fb7239a18a492
Kiranpaulson <kiranplsn2002@gmail.com>
2025-09-23 15:32:53 +0530

    create_epic  added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CreateEpic } from '../components/create-epic/create-epic';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CreateEpic, NgIf],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular_assignment-repo');
+
+  showCreateEpic = false;
+
+  openCreateEpic() {
+    this.showCreateEpic = true;
+    console.log('Create Epic button clicked');
+  }
+
+  closeCreateEpic() {
+    this.showCreateEpic = false;
+    console.log('Create Epic modal closed');
+  }
 }
