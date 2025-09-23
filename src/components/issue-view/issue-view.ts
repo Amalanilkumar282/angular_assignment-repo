a2683416d2e50ee4d4a2b384c82df16d859e1360
Harrel Jacob Alex <harrel.alex@experionglobal.local>
2025-09-23 10:39:43 +0530

    feat: issue view component

---
+++ src/components/issue-view/issue-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-view',
+  imports: [],
+  templateUrl: './issue-view.html',
+  styleUrl: './issue-view.css'
+})
+export class IssueView {
+
+}
