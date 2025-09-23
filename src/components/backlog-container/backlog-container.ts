a130ad21f084f7b4c6e92bb1ae0ff5265383a83d
Amalanilkumar282 <amalanilkumar282@gmail.com>
2025-09-23 10:31:15 +0530

    components directory

---
+++ src/components/backlog-container/backlog-container.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-backlog-container',
+  imports: [],
+  templateUrl: './backlog-container.html',
+  styleUrl: './backlog-container.css'
+})
+export class BacklogContainer {
+
+}
