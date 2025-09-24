4f59bf706bb9964af574491a34c1aa974b88e4a9
Samaspp <samiap914@gmail.com>
2025-09-24 17:40:27 +0530

    Feat: Added drop down functionality

--- src/components/epic-panel-item/epic-panel-item.ts
+++ src/components/epic-panel-item/epic-panel-item.ts
@@ -1,12 +1,18 @@
+import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-epic-panel-item',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './epic-panel-item.html',
   styleUrl: './epic-panel-item.css'
 })
 export class EpicPanelItem {
+  isDropDown = false;
+  onDropDownClick(){
+    this.isDropDown = !this.isDropDown;
+    // console.log(this.isDropDown);
+  }
     @Input() title = '';
     @Input() progress = 0;
     @Input() startDate = '';
