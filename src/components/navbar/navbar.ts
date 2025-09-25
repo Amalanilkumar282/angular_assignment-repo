05c93c778d0b037ed8c536f947c620c1a6c90e2f
NadimNaisam <nadimvc@gmail.com>
2025-09-25 10:07:10 +0530

    Added profile view card

--- src/components/navbar/navbar.ts
+++ src/components/navbar/navbar.ts
@@ -1,21 +1,30 @@
 import { Component ,EventEmitter,Output} from '@angular/core';
+import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
   @Output() createStory = new EventEmitter<void>();
- @Output() createEpic = new EventEmitter<void>()
+  @Output() createEpic = new EventEmitter<void>();
+  showProfileCard = false;
+  userName = 'Linda Smith';
+  userPhoto = 'assets/profile.jpg';
+  userRole = 'Project Manager';
+
   onCreateStoryClick() {
     console.log('Navbar: Create Story clicked');
     this.createStory.emit();
   }
   onCreateEpicClick() {
     console.log('Navbar: Create Epic clicked');
-    this.createEpic.emit(); // 👈 emit event
+    this.createEpic.emit();
+  }
+  toggleProfileCard() {
+    this.showProfileCard = !this.showProfileCard;
   }
 }
